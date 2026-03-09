3c2f38ae3571a0901ff233ba281c333c6c8a6611
titmos <37660314+titmos@users.noreply.github.com>
2026-03-09 22:04:56 +0100

    Add solution using dict comprehension for scores

--- ds_learns.py
+++ ds_learns.py
@@ -1,1 +1,6 @@
+Solution:
 
+# Here is a short solution with a dict comprehension.
+# The lesson gives an example of how to do this with an explicit loop.
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+best_tree_size = min(scores, key=scores.get)

304a397e3a3057691c9b8f49a3f04ae866c60123
titmos <37660314+titmos@users.noreply.github.com>
2026-03-14 18:41:59 +0100

    Update ds_learns.py

--- ds_learns.py
+++ ds_learns.py
@@ -4,3 +4,14 @@
 # The lesson gives an example of how to do this with an explicit loop.
 scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
 best_tree_size = min(scores, key=scores.get)
+
+
+
+Solution: A straightforward (and totally fine) solution is to replace the original print call with:
+if total_candies == 1:
+    print("Splitting 1 candy")
+else:
+    print("Splitting", total_candies, "candies")
+Here's a slightly more succinct solution using a conditional expression:
+
+print("Splitting", total_candies, "candy" if total_candies == 1 else "candies")

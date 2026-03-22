a03c43426d0fdbc820972aaafe12137fb9e0dc19
titmos <37660314+titmos@users.noreply.github.com>
2026-03-22 22:51:08 +0100

    Update ds_learns.py

--- ds_learns.py
+++ ds_learns.py
@@ -110,3 +110,27 @@ def word_search(doc_list, keyword):
     pass
 # Check your answer
 q2.check()
+
+
+'''We'd like to host these wine reviews on our website, but a rating system ranging from 80 to 100 points is too hard to understand - we'd like to translate them into simple star ratings. A score of 95 or higher counts as 3 stars, a score of at least 85 but less than 95 is 2 stars. Any other score is 1 star.
+
+Also, the Canadian Vintners Association bought a lot of ads on the site, so any wines from Canada should automatically get 3 stars, regardless of points.
+
+Create a series star_ratings with the number of stars corresponding to each review in the dataset.'''
+
+
+def ratings(row):
+    if row.country == 'Canada':
+        return 3
+    elif row.points >= 95:
+        return 3
+    elif row.points >= 85:
+        return 2
+    else:
+        return 1
+    pass
+star_ratings = reviews.apply(ratings, axis='columns')
+
+# Check your answer
+q7.check()
+star_ratings
